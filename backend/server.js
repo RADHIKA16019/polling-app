2f6ccdcddd7b86936c76033614ce1da57e199612
RADHIKA16019 <radhika16019@gmail.com>
2026-05-15 23:06:13 +0530

    add PORT to server

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,7 @@ import pollRoutes from "./routes/pollRoutes.js";
 
 const app = express();
 const server = http.createServer(app);
+const PORT = process.env.PORT || 5000;
 
 connectDB();
 
@@ -50,6 +51,6 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(5000, () => {
-  console.log("Server running on port 5000");
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
